9bea0230a4d45f620107f727f61e4e351fc9b82b
KwabenaYeboah <kwabena4140@gmail.com>
2021-12-06 11:10:15 +0000

    Created models for each type of course content(file,image,video,text)

--- courses/models.py
+++ courses/models.py
@@ -1,4 +1,5 @@
-from typing import overload
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -34,5 +35,38 @@ class Module(models.Model):
     def __str__(self):
         return self.title
     
+class Content(models.Model):
+    module = models.ForeignKey(Module, related_name='contents', on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE,
+                                     limit_choices_to={'model__in':('text','video',
+                                                                    'file', 'image')})
+    object_id = models.PositiveIntegerField()
+    content = GenericForeignKey('content_type', 'object_id')    
+
+# Customizing all the various kinds of contents with Abstract model 
+class ContentBase(models.Model):
+    instructor = models.ForeignKey(User, related_name='%(class)s_related', on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        abstract = True
+        
+    def __str__(self):
+        return self.title
+
+# The following content classes all inherits from ContentBase
+class Text(ContentBase):
+    content = models.TextField()
+    
+class File(ContentBase):
+    file = models.FileField(upload_to='files')
+
+class Image(ContentBase):
+    file = models.FileField(upload_to='images')
+
+class Vide(ContentBase):
+    url = models.URLField()
     
     
