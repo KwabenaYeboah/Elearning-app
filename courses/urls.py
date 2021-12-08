f288ab0c940b2d44368750a95f26beeb8bfcc1e1
KwabenaYeboah <kwabena4140@gmail.com>
2021-12-08 14:22:31 +0000

    Added formsets for course modules

--- courses/urls.py
+++ courses/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from .views import CourseListView, CourseCreateView, CourseUpdateView, CourseDeleteView
+from .views import (CourseListView, CourseCreateView, 
+                    CourseUpdateView, CourseDeleteView,
+                    CourseModuleUpdateView)
 
 urlpatterns = [
     path('your-courses/', CourseListView.as_view(), name='course_list'),
     path('create/', CourseCreateView.as_view(), name='course_create'),
     path('<pk>/update/', CourseUpdateView.as_view(), name='course_update'),
     path('<pk>/delete/', CourseDeleteView.as_view(), name='course_delete'),
+    path('<pk>/module/', CourseModuleUpdateView.as_view(), name='course_module_update'),
+    
 ]
