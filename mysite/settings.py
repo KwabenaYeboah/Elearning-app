974e98d99405387e197f868f943a3337ef0c8ff8
KwabenaYeboah <kwabena4140@gmail.com>
2021-12-11 10:14:54 +0000

    Added signup functionality for Students

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Local apps
     'courses',
+    'students',
 ]
 
 MIDDLEWARE = [
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Accra'
 
 USE_I18N = True
 
