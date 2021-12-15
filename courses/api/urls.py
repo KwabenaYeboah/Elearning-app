aabb6fa426e7bb9e96a5a9d7f83fb22dc77e7e4c
KwabenaYeboah <kwabena4140@gmail.com>
2021-12-15 08:47:51 +0000

    Added API viewsets and routers

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
+from django.urls import path, include 
+from rest_framework import routers
 
-from .views import SubjectListView, SubjectDetailView, CourseEnrollView
+from .views import (SubjectListView, SubjectDetailView,
+                    CourseViewSet,)
 
+router = routers.DefaultRouter()
+router.register('courses', CourseViewSet)
 urlpatterns = [
     path('subjects/', SubjectListView.as_view(), name='subject_list'),
     path('subjects/<pk>/', SubjectDetailView.as_view(), name='subject_detail'),
-    path('courses/<pk>/enroll/', CourseEnrollView.as_view(), name='course_enroll'),
+    #path('courses/<pk>/enroll/', CourseEnrollView.as_view(), name='course_enroll'),
+    path('', include(router.urls)),
 ]
