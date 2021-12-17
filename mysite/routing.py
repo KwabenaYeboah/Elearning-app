d42ef3df3f8ece045826cc7a5f57a821533b4dc8
KwabenaYeboah <kwabena4140@gmail.com>
2021-12-17 11:09:12 +0000

    Added a WebSocket client using jQuery

--- mysite/routing.py
+++ mysite/routing.py
@@ -1,10 +1,10 @@
-from channels.auth import AuthMiddleWareStack
+from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter 
 
 from forum import routing
 
 application = ProtocolTypeRouter({
-    'websocket':AuthMiddleWareStack(
+    'websocket':AuthMiddlewareStack(
         URLRouter(routing.websocket_urlpatterns)
     ),
 })
