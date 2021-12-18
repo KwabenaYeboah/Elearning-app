3e206c75267c95c0bd3dab3238ae23a2fe367d97
KwabenaYeboah <kwabena4140@gmail.com>
2021-12-18 09:23:21 +0000

    Integrated Channel layer using channels-redis

--- forum/consumers.py
+++ forum/consumers.py
@@ -1,16 +1,32 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 
 class ForumConsumer(WebsocketConsumer):
     def connect(self):
+        self.id = self.scope['url_route']['kwargs']['course_id']
+        self.course_forum_name = 'course_forum_%s' % self.id
+        # join course forum
+        async_to_sync(self.channel_layer.group_add)(
+            self.course_forum_name, self.channel_name)
         # Accept connection
         self.accept()
     
     def disconnect(self, close_code):
-        pass
+        # leanve course forum
+        async_to_sync(self.channel_layer.group_discard)(
+            self.course_forum_name, self.channel_name)
+        
     # receive message from webSocket
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        # send message to course forum
+        async_to_sync(self.channel_layer.group_send)(
+            self.course_forum_name, {'type':'chat_message', 'message':message}
+        )
+    
+    # Receive message from course forum
+    def chat_message(self, event):
         # send message to Websocket
-        self.send(text_data=json.dumps({'message':message})) 
+        self.send(text_data=json.dumps(event))
--- mysite/settings.py
+++ mysite/settings.py
@@ -165,3 +165,12 @@ REST_FRAMEWORK = {
 
 # Channel config 
 ASGI_APPLICATION = 'mysite.routing.application'
+
+CHANNEL_LAYERS = {
+    'default':{
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts':[('127.0.0.1', 6379)],
+        },
+    },
+}
