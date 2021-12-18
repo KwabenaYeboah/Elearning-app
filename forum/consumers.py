488231d2bba191a0c7ec315b96eda234648da33e
KwabenaYeboah <kwabena4140@gmail.com>
2021-12-18 10:25:37 +0000

    Added forum link

--- forum/consumers.py
+++ forum/consumers.py
@@ -1,31 +1,31 @@
 import json
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import async_to_sync
 from django.utils import timezone 
 
-class ForumConsumer(WebsocketConsumer):
-    def connect(self):
+class ForumConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.user = self.scope['user']
         self.id = self.scope['url_route']['kwargs']['course_id']
         self.course_forum_name = 'course_forum_%s' % self.id
         # join course forum
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.course_forum_name, self.channel_name)
         # Accept connection
-        self.accept()
+        await self.accept()
     
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         # leanve course forum
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_discard(
             self.course_forum_name, self.channel_name)
         
     # receive message from webSocket
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
         now = timezone.now()
         # send message to course forum
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.course_forum_name, {'type':'chat_message', 
                                      'message':message,
                                      'user':self.user.username,
@@ -33,6 +33,6 @@ class ForumConsumer(WebsocketConsumer):
         )
     
     # Receive message from course forum
-    def chat_message(self, event):
+    async def chat_message(self, event):
         # send message to Websocket
-        self.send(text_data=json.dumps(event))
+        await self.send(text_data=json.dumps(event))
